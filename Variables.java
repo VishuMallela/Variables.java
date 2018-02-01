eb8de13f60224cd986b2544d2aba9b53fe32d07b
Vishu Mallela <vishumallela@icloud.com>
2018-02-01 21:34:48 +0530

    Added the code
    Imported the code via mac

---
+++ Variables.java
@@ -0,0 +1,17 @@
+/* Program used to print two variables using basic operators */
+
+public class Variables { // File name is Variables.java
+public static void main(String args[]) { // Code begins executing from here
+int var1; // Introducing integer variable var1
+int var2; // Introducing integer variable var2
+
+var1 = 1024; // Defining value of var1
+
+System.out.println("Variable 1 contains " + var1); // Printing var1
+
+var2= var1 / 2; // Defining value of var2
+
+System.out.print("Variable 2 coutains Variable 1 / 2 = "); // Printing var2
+System.out.println(var2);
+}
+}
